81edefb656533d1fd9cbeb1603ce6a77844951c2
Tiago Cintra <tiago.slv@hotmail.com>
2023-07-02 20:58:20 -0300

    refactor(footerwidgets): cases and materials molecules refactored to add links and title

--- src/components/molecules/Footer/Cases.tsx
+++ src/components/molecules/Footer/Cases.tsx
@@ -1,7 +1,16 @@
-interface CasesProps {}
+import { CustomLink, Title } from '@/components/atoms'
 
-export const Cases = ({}:CasesProps) => {
+const links = ['Geração de Leads B2B', 'Geração de Leads em Software', 'Geração de Leads em Imobiliária', 'Cases de Sucesso']
+
+export const Cases = () => {
   return (
-    <div>Cases</div>
+    <section className='flex flex-col gap-8'>
+      <Title classname='mb-6'>Cases</Title>
+      {links.map((link) => (
+        <CustomLink key={link} href={`#${link}`}>
+          {link}
+        </CustomLink>
+      ))}
+    </section>
   )
 }
--- src/components/molecules/Footer/Materials.tsx
+++ src/components/molecules/Footer/Materials.tsx
@@ -1,7 +1,21 @@
-interface MaterialsProps {}
+import { CustomLink, Title } from '@/components/atoms'
 
-export const Materials = ({}:MaterialsProps) => {
+const links = [
+  'Blog',
+  'Parceria com Agências',
+  'Guia Definitivo do Marketing',
+  'Materiais Gratuitos',
+]
+
+export const Materials = () => {
   return (
-    <div>Materials</div>
+    <section className="flex flex-col gap-8">
+      <Title classname="mb-6">Materiais</Title>
+      {links.map((link) => (
+        <CustomLink key={link} href={`#${link}`}>
+          {link}
+        </CustomLink>
+      ))}
+    </section>
   )
 }
